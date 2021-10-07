314ec14ac3bb48d0468bb6e239919baddb9c394b
LazyPanda397 <gauravr397@gmail.com>
2021-10-07 13:48:01 +0530

    exception handling //

---
+++ qwrt.java
@@ -0,0 +1,20 @@
+public class qwrt {
+    void div(int a, int b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException();
+        } else {
+            int c = a / b;
+            System.out.println(c);
+        }
+    }
+
+    public static void main(String[] args) {
+        qwrt t = new qwrt();
+        try {
+            t.div(20, 0);
+        } catch (Exception e) {
+            System.out.println("b is zero");
+        }
+
+    }
+}
