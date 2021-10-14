7c4a118fabecaa2faffc23223a481f7dbbed4d7e
LazyPanda397 <gauravr397@gmail.com>
2021-10-14 12:08:15 +0530

    /

--- fibonacci.java
+++ fibonacci.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 class fibonacci {
     public static void main(String[] args) {
-        int range, a = 0, b = 1, c;
+        int range, a = 2, b = 3, c;
         System.out.print("Enter the range ");
         Scanner r = new Scanner(System.in);
         range = r.nextInt();
         for (int i = 1; i <= range; i++) {
-            System.out.print(a+" ");
+            System.out.print(a + " ");
             c = a + b;
             a = b;
             b = c;
